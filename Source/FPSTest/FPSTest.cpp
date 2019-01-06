fb84109f80055ee0bea17e3af68cf54ec52cddcc
Patrick Dildine <wpdildine@gmail.com>
2019-01-06 17:59:46 -0500

    Initial Commit

---
+++ Source/FPSTest/FPSTest.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "FPSTest.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FPSTest, "FPSTest" );
+ 
---
+++ Source/FPSTest/FPSTestHUD.h
@@ -0,0 +1,25 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once 
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "FPSTestHUD.generated.h"
+
+UCLASS()
+class AFPSTestHUD : public AHUD
+{
+	GENERATED_BODY()
+
+public:
+	AFPSTestHUD();
+
+	/** Primary draw call for the HUD */
+	virtual void DrawHUD() override;
+
+private:
+	/** Crosshair asset pointer */
+	class UTexture2D* CrosshairTex;
+
+};
+
