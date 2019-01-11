b9f9456dbfab4580947ccd450ff522082fd0d17d
Patrick Dildine <wpdildine@gmail.com>
2019-01-10 20:20:24 -0500

    updated key

--- Source/FPSTest/Private/AITaskNode.cpp
+++ Source/FPSTest/Private/AITaskNode.cpp
@@ -1,9 +1,13 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "../Public/AITaskNode.h"
+#include "Runtime/AIModule/Classes/BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UAITaskNode::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
 {
+
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	UE_LOG(LogTemp, Warning, TEXT("Waypoint index %i"), Index)
 	return EBTNodeResult::Succeeded;
 }
--- Source/FPSTest/Public/AITaskNode.h
+++ Source/FPSTest/Public/AITaskNode.h
@@ -15,4 +15,8 @@ class FPSTEST_API UAITaskNode : public UBTTaskNode
 	GENERATED_BODY()
 
 	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+		struct FBlackboardKeySelector IndexKey;
 };
